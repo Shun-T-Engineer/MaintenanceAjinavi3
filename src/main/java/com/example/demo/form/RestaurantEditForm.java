5be7f5ee16e5039c3ea00375c8c86b94aaca83ed
Shun-T-Engineer <shun.0520.1988@gmail.com>
2025-07-06 15:01:33 +0900

    RestaurantEditForm.javaの作成

--- src/main/java/com/example/demo/form/RestaurantEditForm.java
+++ src/main/java/com/example/demo/form/RestaurantEditForm.java
@@ -1,12 +1,15 @@
 package com.example.demo.form;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
-public class ShopRegistForm {
+public class RestaurantEditForm {
+	@NotNull(message="入力してください。")
+	@Min(value=1,  message="正の整数を入力してください。")
 	private int restaurantId;
 	
 	@NotNull(message="入力してください")
